82474b6527a4fa375ff0e4d3a8223d8c48568eee
orumyn <o.rumyn@google.com>
2025-03-28 03:50:16 +0200

    Added JS homework 7_2

--- homework7.js
+++ homework7.js
@@ -1,3 +1,36 @@
 const text = "Wonderful, Joyful, Happiness, Time, Task, Apple";
 const regex = /\b[^Aa,\s]{6,}\b/g;
 console.log(text.match(regex));
+
+
+// друга частина домашньоъ роботи
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com" // прибрав <> , було записано <email:"dmitro.porohov@yahoo.com>"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+
+var re = /[\w]+(?:\.[\w]+)?@(gmail\.com|yahoo\.com)$/;
+
+var arrNew = arr
+            .filter(user  => re.test(user.email))
+            .map(user => ({
+                userName: user.userName,
+                lastName: user.lastName,
+                email: user.email
+            }));
+
+console.log(arrNew);
